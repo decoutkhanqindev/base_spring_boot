b2b4075ef923813bcc0b07e707817a17200ba874
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-07-26 20:02:20 +0700

    first commit

---
+++ db_connect_user_crud/src/main/java/com/example/db_connect_user_crud/DbConnectUserCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.db_connect_user_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbConnectUserCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbConnectUserCrudApplication.class, args);
+	}
+
+}
---
+++ db_connect_user_crud/src/test/java/com/example/db_connect_user_crud/DbConnectUserCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.db_connect_user_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbConnectUserCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
