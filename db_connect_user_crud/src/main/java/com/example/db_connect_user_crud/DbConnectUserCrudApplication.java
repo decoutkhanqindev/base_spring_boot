7f604e33273164349fe779e7d6443330530b4c65
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-07-26 22:31:52 +0700

    feat: config application.yaml to define server properties

--- db_connect_user_crud/src/main/java/com/example/db_connect_user_crud/DbConnectUserCrudApplication.java
+++ db_connect_user_crud/src/main/java/com/example/db_connect_user_crud/DbConnectUserCrudApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DbConnectUserCrudApplication {
-
   public static void main(String[] args) {
     SpringApplication.run(DbConnectUserCrudApplication.class, args);
   }
-
 }
