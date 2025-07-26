35dc307e95ee59cf32a563dbd9732ac80afbddf7
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-07-26 23:36:29 +0700

    feat: add UserRequest DTO for user data transfer

---
+++ db_connect_user_crud/src/main/java/com/example/db_connect_user_crud/dto/request/UserRequest.java
@@ -0,0 +1,41 @@
+package com.example.db_connect_user_crud.dto.request;
+import java.time.LocalDate;
+
+public class UserRequest {
+  private String username;
+  private String password;
+  private String email;
+  private LocalDate dob;
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public LocalDate getDob() {
+    return dob;
+  }
+
+  public void setDob(LocalDate dob) {
+    this.dob = dob;
+  }
+}
