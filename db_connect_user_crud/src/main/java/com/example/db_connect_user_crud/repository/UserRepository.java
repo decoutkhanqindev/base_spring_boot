96442468ec63af18bd19ad76e4f93749a8015056
decoutkhanqindev <phamminhkhangpy2003@gmail.com>
2025-07-26 23:36:38 +0700

    feat: add UserRepository interface for user data access

---
+++ db_connect_user_crud/src/main/java/com/example/db_connect_user_crud/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.db_connect_user_crud.repository;
+
+import com.example.db_connect_user_crud.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+//  JpaRepository<User, Long> -> User - Entity and String is ID type
+}
