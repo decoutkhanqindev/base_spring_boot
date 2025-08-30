a3625c697cf2c5738d4b0556eb07fedd4a0346e3
Minh Khang <phamminhkhangpy2003@gmail.com>
2025-08-30 22:24:42 +0700

    feat: add OAuth2 resource server configuration and JWT authentication support

---
+++ db_connect_user_crud/src/main/java/com/example/db_connect_user_crud/config/SecurityConfig.java
@@ -0,0 +1,58 @@
+package com.example.db_connect_user_crud.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+
+import javax.crypto.spec.SecretKeySpec;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+  // public endpoints are allowed to be accessed without authentication
+  private final String[] PUBLIC_ENDPOINTS = {"/user", "/auth/login", "/auth/introspect"};
+
+  @Value("${jwt.secret-key}")
+  private String SECRET_KEY;
+
+  @Bean
+  // define a security filter chain for endpoints
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    // configure a security filter chain for endpoints
+    http.authorizeHttpRequests(request ->
+      request
+        .requestMatchers(HttpMethod.POST, PUBLIC_ENDPOINTS)
+        .permitAll()
+        .anyRequest()
+        .authenticated()
+    );
+
+    // configure oauth2 resource server for jwt authentication
+    http.oauth2ResourceServer(oauth2Configurer ->
+      oauth2Configurer.jwt(jwtConfigurer -> jwtConfigurer.decoder(jwtDecoder()))
+    );
+
+    // disable csrf (cross site request forgery)
+    http.csrf(AbstractHttpConfigurer::disable);
+
+    return http.build();
+  }
+
+  @Bean
+  // define a jwt decoder to verify jwt token
+  public JwtDecoder jwtDecoder() {
+    SecretKeySpec secretKeySpec = new SecretKeySpec(SECRET_KEY.getBytes(), "HS512");
+    return NimbusJwtDecoder
+      .withSecretKey(secretKeySpec)
+      .macAlgorithm(MacAlgorithm.HS512)
+      .build();
+  }
+}
